6010f9b24a0ac19923fe1cbaa46bd1a4eb37cb6b
Koushik Dey <koushik.dey@live.com>
2022-04-28 00:20:54 +0300

    fy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,96 @@ import './design.js';
 const App = () => {
   const [breakLength, setBreakLength] = React.useState(5);
   const [sessionLength, setSessionLength] = React.useState(25);
-  const { handleBreakIncrease, handleBreakDecrease, handleSessionIncrease, handleSessionDecrease, title, handlePlay, handleReset, play, timeFormatter = () => "25:00" } = {}
+   const [timeLeft, seTtimeLeft] = React.useState(1500);
+  const [timingType, setTimingtype] = React.useState("SESSION");
+  const [play, setPlay] = React.useState(false);
+
+  const timeout = setTimeout(() => {
+    if(timeLeft && play){
+      seTtimeLeft(timeLeft - 1)
+    }
+  }, 1000);
+
+  const handleBreakIncrease = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1)
+    }
+  }
+const handleBreakDecrease = () => {
+    if(breakLength > 1){
+      setBreakLength(breakLength - 1)
+    }
+  }
+
+   const handleSessionIncrease = () => {
+    if(sessionLength < 60){
+      setSessionLength(sessionLength + 1)
+      seTtimeLeft(timeLeft + 60)
+    }
+  }
+
+  const handleSessionDecrease = () => {
+    if(sessionLength > 1){
+      setSessionLength(sessionLength - 1)
+      seTtimeLeft(timeLeft - 60)
+    }
+  }
+
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    seTtimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingtype("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause()
+    audio.currentTime = 0;
+  }
+
+  const handlePlay = () => {
+    clearTimeout(timeout);
+    setPlay(!play);
+  }
+
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if(!timeLeft && timingType === "SESSION"){
+      seTtimeLeft(breakLength * 60)
+      setTimingtype("BREAK")
+      audio.play()
+    }
+    if(!timeLeft && timingType === "BREAK"){
+      seTtimeLeft(sessionLength * 60)
+      setTimingtype("SESSION")
+      audio.pause()
+      audio.currentTime = 0;
+    }
+  }
+
+  const clock = () => {
+    if(play){
+      timeout
+      resetTimer()
+    } else {
+      clearTimeout(timeout)
+    }
+  }
+
+  React.useEffect(() => {
+    clock()
+  }, [play, timeLeft, timeout])
+
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  const title = timingType === "SESSION" ? "Session" : "Break";
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,24 +105,24 @@ const App = () => {
          </div>
        </div>
       </header>
-      <div className="background">
+     <div className="background">
       <div className="wrapper">
       <h2>25 + 5 Clock</h2>
       <div className="break-session-length">
         <div>
           <h3 id="break-label">Break Length</h3>
           <div>
-            <button disabled={play} onClick={handleBreakIncrease} id="break-increment">Increase</button>
+            <button disabled={play} onClick={handleBreakIncrease} id="break-increment">+</button>
               <strong id="break-length">{breakLength}</strong>
-            <button disabled={play} onClick={handleBreakDecrease} id="break-decrement">Decrease</button>
+            <button disabled={play} onClick={handleBreakDecrease} id="break-decrement">-</button>
           </div>
          </div>
          <div>
            <h3 id="session-label">Session Length</h3>
            <div>
-            <button disabled={play} onClick={handleSessionIncrease} id="session-increment">Increase</button>
+            <button disabled={play} onClick={handleSessionIncrease} id="session-increment">+</button>
               <strong id="session-length">{sessionLength}</strong>
-            <button disabled={play} onClick={handleSessionDecrease} id="session-decrement">Decrease</button>
+            <button disabled={play} onClick={handleSessionDecrease} id="session-decrement">-</button>
           </div>
          </div>
       </div>
